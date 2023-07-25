3aef3615b7993f8651cd491600a620ddce7cd97d
3000ye <hcg2924239214@163.com>
2023-07-25 13:03:51 +0800

    切换到ubuntu中配置

--- test.cpp
+++ test.cpp
@@ -1,21 +1,7 @@
-#include <bits/stdc++.h>
-#include <bits/types/clock_t.h>
-#include <ctime>
+#include "bits/stdc++.h"
 
 using namespace std;
-typedef long long ll;
-typedef long double ld;
-
-int a;
-typedef int b;
-
-void solve() {
-    
-}
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    solve();
-    return 0;
+    vector<int> 
 }
