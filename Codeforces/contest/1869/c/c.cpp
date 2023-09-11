6e9713462bb5c97c339e503ceab0a6bb780c9fa0
3000ye <hcg2924239214@163.com>
2023-09-11 20:12:10 +0800

    新增了Codeforces div2

--- Codeforces/contest/1869/c/c.cpp
+++ Codeforces/contest/1869/c/c.cpp
@@ -10,11 +10,7 @@ void solve() {
     int n, m; cin >> n >> m;
     
     for (int i = 0; i < n; i ++) {
-        cout << i << " ";
-        for (int j = 0; j < m; j ++) {
-            if (j != i) cout << j << " ";
-        }
-        cout << endl;
+        cout << 
     }
 }
 
